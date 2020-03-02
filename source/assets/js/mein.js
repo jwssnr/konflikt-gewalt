68ea6de968dc0707f75fff902c5e3b0d7ccffbdf
irisgraser <grafik@irisgraser.ch>
2020-03-02 10:51:25 +0100

    Browserwarnung

--- source/assets/js/mein.js
+++ source/assets/js/mein.js
@@ -23,4 +23,20 @@
       $(".mobileclass").toggleClass("menuoffen");
     });
 
+    var rhabarber = '<div class="browserblast">';
+    rhabarber += '<h2>:-(</h2>';
+    rhabarber += '<p>Ihr Browser stammt aus geraumer Vorzeit und ist zu alt, um unsere Website sauber darzustellen und sicher zu nutzen.</p>';
+    rhabarber += '<p>Wir empfehlen, ein <a href="https://www.microsoft.com/en-us/edge" target="_blank">Upgrade</a> oder einen Wechsel auf <a href="https://www.firefox.com" target="_blank">Firefox</a> oder <a href="https://www.google.com/chrome" target="_blank">Google Chrome</a> vorzunehmen.</p>';
+    rhabarber += '<p class="absender">Ihr Team von KONFLIKT.GEWALT.</p>';
+    rhabarber += '<p class="browserblast__closer">×</p>';
+    rhabarber += '</div>';
+    var ua = window.navigator.userAgent;
+    var isIE = /MSIE|Trident/.test(ua);
+    if ( isIE ) {
+      document.body.innerHTML += rhabarber;
+    }
+    $(".browserblast__closer").click(function(){
+      $(".browserblast").fadeOut();
+    });
+
   });
